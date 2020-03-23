8b1b19e4cd2beecd6e5c1c5d3072304f4fdaf4f4
anton <antonio.brandi@outlook.it>
2020-03-23 08:57:04 +0100

    init of the cleaning project

---
+++ Android/Im-gine App/app/src/main/java/com/example/im_gine/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.im_gine;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
